51786dc84e31933aeeb9c5a048df4d506028c7cb
alnero <darestream@gmail.com>
2021-08-18 16:22:51 +0300

    Correct text column data type

--- src/main/resources/db/post.sql
+++ src/main/resources/db/post.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post (
     id      SERIAL PRIMARY KEY,
     name    VARCHAR(255),
-    text    VARCHAR(255),
+    text    TEXT,
     link    VARCHAR(255) UNIQUE,
     created TIMESTAMP
 );
